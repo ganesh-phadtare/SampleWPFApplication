4ee37bc62fb13d0792ab3dc55c97bf3c9c6dfe2a
ganesh-phadtare <ganesh.phadtare@v2solutions.com>
2017-01-24 13:16:00 +0530

    My first checkin
    My first checkin

---
+++ SampleWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+
+namespace SampleWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ApplicationConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionString);
+            if (con.State != System.Data.ConnectionState.Open)
+                con.Open();
+            MessageBox.Show("Connection Tested.... ");
+            con.Close();
+        }
+
+    }
+}
